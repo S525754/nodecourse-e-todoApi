c6cdc2de9a33e5cb88e3741a612a0428d304e2d1
Vinod Kumar Reddy <S525754@mail.nwmissouri.edu>
2017-06-21 18:39:13 -0500

    Add DELETE /todos/}

--- server/server.js
+++ server/server.js
@@ -53,6 +53,24 @@ app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
+app.delete('/todos/:id', (req, res) => {
+   var id = req.params.id;
+
+   if (!ObjectID.isValid(id)) {
+       return res.status(404).send();
+   }
+
+   Todo.findByIdAndRemove(id).then((todo) => {
+       if(!todo) {
+           return res.status(404).send();
+       }
+
+       res.send(todo);
+   }).catch((err) => {
+       res.status(400).send();
+   })
+});
+
 
 module.exports = {app};
 
