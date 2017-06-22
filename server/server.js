bf70a2c9edb25754a4d2a4d66cb493b8cea2e8aa
Vinod Kumar Reddy <S525754@mail.nwmissouri.edu>
2017-06-21 20:26:30 -0500

    Add PATCH /todos/ :id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -48,28 +49,55 @@ app.get('/todos/:id', (req, res) => {
    });
 });
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((err) => {
+        res.status(400).send();
+    })
+});
+
+    app.patch('/todos/:id', (req, res) => {
+        var id = req.params.id;
+        var body = _.pick(req.body, ['text', 'completed']);
+
+        if (!ObjectID.isValid(id)) {
+            return res.status(404).send();
+        }
+
+        if (_.isBoolean(body.completed) && body.completed) {
+            body.completedAt = new Date().getTime();
+        } else {
+            body.completed = false;
+            body.completedAt = null;
+        }
+
+        Todo.findByIdAndUpdate(id, {$set: body}, {new: false}).then((todo) => {   // new in mongoose like returnOriginal in mongodb
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.send({todo});
+
+        }).catch((err) =>{
+            res.status(400).send()
+        })
+    });
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
-app.delete('/todos/:id', (req, res) => {
-   var id = req.params.id;
-
-   if (!ObjectID.isValid(id)) {
-       return res.status(404).send();
-   }
-
-   Todo.findByIdAndRemove(id).then((todo) => {
-       if(!todo) {
-           return res.status(404).send();
-       }
-
-       res.send({todo});
-   }).catch((err) => {
-       res.status(400).send();
-   })
-});
 
 
 module.exports = {app};
